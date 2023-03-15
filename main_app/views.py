e98c46818b3ee5d9201a1883d76ace88bc1d3dfd
ben-manning <bmanning@me.com>
2023-03-15 15:37:03 -0400

    add index page for finches

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+finches = [
+  { 'name': 'Lucy', 'breed': 'Cardinal', 'description': 'this bird is red'},
+  { 'name': 'Harvey', 'breed': 'Blue Jay', 'description': 'this bird is Canadian' }
+]
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
