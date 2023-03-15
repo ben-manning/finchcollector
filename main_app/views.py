b8b686ea77af8c76e4d187f53864c05e85540376
ben-manning <bmanning@me.com>
2023-03-15 16:03:42 -0400

    run migration and connect model

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
-finches = [
-  { 'name': 'Lucy', 'breed': 'Cardinal', 'description': 'this bird is red'},
-  { 'name': 'Harvey', 'breed': 'Blue Jay', 'description': 'this bird is Canadian' }
-]
+from .models import Finch
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
@@ -12,6 +9,7 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', {
     'finches': finches
   })
