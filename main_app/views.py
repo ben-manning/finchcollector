ea06b5dbe5c3226ca0620222af2d78e776441fec
ben-manning <bmanning@me.com>
2023-03-16 15:04:50 -0400

    create crud functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 # Create your views here.
 def home(request):
@@ -17,3 +18,15 @@ def finches_index(request):
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   return render(request, 'finches/detail.html', {'finch': finch})
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = '__all__'
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
