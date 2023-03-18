f69aa193b68d75672dc42ac976dee54ed583d6df
ben-manning <bmanning@me.com>
2023-03-18 14:00:31 -0400

    show associated model in finch

--- main_app/models.py
+++ main_app/models.py
@@ -22,6 +22,7 @@ class Finch(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
+  toys = models.ManyToManyField(Toy)
 
   def __str__(self):
     return self.name
